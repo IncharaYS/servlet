e8ec8330e550746fd2537800f91231bae89fe7fc
IncharaYS <218554110+IncharaYS@users.noreply.github.com>
2025-12-07 12:49:20 +0530

    Add .gitignore and initial project configuration files

--- bon-voyage-system/src/main/java/com/xworkz/travelAgencyApp/repository/TravelAgencyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.xworkz.travelAgencyApp.repository;
-
-import com.xworkz.travelAgencyApp.dto.TravelAgencyDTO;
-
-public interface TravelAgencyRepository {
-    boolean save(TravelAgencyDTO travelAgencyDTO);
-}
